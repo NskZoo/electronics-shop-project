2fc2d67c703544238065bbbc44e5aeb6a7f5372a
Oleg Sorokin <ole66112@gmail.com>
2024-03-02 06:05:08 +0300

    дз 5.1 доработка

--- src/keyboard.py
+++ src/keyboard.py
@@ -10,6 +10,9 @@ class Lang:
         return self.__language
 
     def change_lang(self):
+        """
+        Метод изменения языка клавиатуры
+        """
         if self.__language == "EN":
             self.__language = "RU"
         else:
@@ -17,9 +20,15 @@ class Lang:
 
 
 class Keyboard(Item, Lang):
+    """
+    Дочерний класс для товара "клавиатура"
+    """
     def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
         Lang.__init__(self)
 
     def __str__(self) -> str:
+        """
+        Метод, который выводит информацию для пользователей
+        """
         return self.name
